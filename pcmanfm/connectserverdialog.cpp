27a1aaf25f7e1d7fd2ff376d1e3c96afd0a2dedd
Paul <p@1g4.org>
2025-11-29 11:57:28 -0600

    pcmanfm-qt: tidy connect server dialog and input validation
    - Initialize server type table in the constructor initializer list
    - Remove unnecessary const_cast and iterate server types directly
    - Guard against invalid server type indices when building URIs and updating UI
    - Make input validation a bit more defensive and avoid null dereferences on the OK button

--- pcmanfm/connectserverdialog.cpp
+++ pcmanfm/connectserverdialog.cpp
@@ -1,86 +1,113 @@
 #include "connectserverdialog.h"
+
+#include <QComboBox>
+#include <QDialogButtonBox>
+#include <QLineEdit>
 #include <QMessageBox>
 #include <QPushButton>
 
 namespace PCManFM {
 
-ConnectServerDialog::ConnectServerDialog(QWidget *parent): QDialog(parent) {
-  serverTypes = QList<ServerType>{
-    {tr("SSH"), "sftp", 22, false},
-    {tr("FTP"), "ftp", 21, true},
-    {tr("WebDav"), "dav", 80, true},
-    {tr("Secure WebDav"), "davs", 443, false},
-    {tr("HTTP"), "http", 80, true},
-    {tr("HTTPS"), "https", 443, true},
-  };
+ConnectServerDialog::ConnectServerDialog(QWidget* parent)
+    : QDialog(parent),
+      serverTypes{
+          {tr("SSH"), "sftp", 22, false},  {tr("FTP"), "ftp", 21, true},
+          {tr("WebDav"), "dav", 80, true}, {tr("Secure WebDav"), "davs", 443, false},
+          {tr("HTTP"), "http", 80, true},  {tr("HTTPS"), "https", 443, true},
+      } {
+    ui.setupUi(this);
 
-  ui.setupUi(this);
+    for (const auto& serverType : serverTypes) {
+        ui.serverType->addItem(serverType.name);
+    }
 
-  connect(ui.serverType, &QComboBox::currentIndexChanged, this, &ConnectServerDialog::onCurrentIndexChanged);
+    connect(ui.serverType, &QComboBox::currentIndexChanged, this, &ConnectServerDialog::onCurrentIndexChanged);
 
-  connect(ui.host, &QLineEdit::textChanged, this, &ConnectServerDialog::checkInput);
-  connect(ui.userName, &QLineEdit::textChanged, this, &ConnectServerDialog::checkInput);
-  for(const auto& serverType : const_cast<const QList<ServerType>&>(serverTypes)) {
-    ui.serverType->addItem(serverType.name);
-  }
+    connect(ui.host, &QLineEdit::textChanged, this, &ConnectServerDialog::checkInput);
+    connect(ui.userName, &QLineEdit::textChanged, this, &ConnectServerDialog::checkInput);
 
-  ui.serverType->setCurrentIndex(0);
-  onCurrentIndexChanged(0);
+    ui.serverType->setCurrentIndex(0);
+    onCurrentIndexChanged(0);
 }
 
 ConnectServerDialog::~ConnectServerDialog() = default;
 
-
 QString ConnectServerDialog::uriText() {
-  QString uri;
-  int serverTypeIdx = ui.serverType->currentIndex();
-  const auto& serverType = serverTypes[serverTypeIdx];
+    const int serverTypeIdx = ui.serverType->currentIndex();
+    if (serverTypeIdx < 0 || serverTypeIdx >= serverTypes.size()) {
+        return {};
+    }
 
-  // make an URI from the data
-  uri = QString::fromLatin1(serverType.scheme);
-  uri += QStringLiteral("://");
-  if(ui.loginAsUser->isChecked()) {
-    uri += ui.userName->text().trimmed();
-    uri += QLatin1Char('@');
-  }
+    const auto& serverType = serverTypes[serverTypeIdx];
 
-  uri += ui.host->text().trimmed();
-  int port = ui.port->value();
-  if(port != serverType.defaultPort) {
-    uri += QLatin1Char(':');
-    uri += QString::number(port);
-  }
+    QString uri = QString::fromLatin1(serverType.scheme);
+    uri += QStringLiteral("://");
 
-  QString path = ui.path->text();
-  if(path.isEmpty() || path[0] != QLatin1Char('/')) {
-    uri += QLatin1Char('/');
-  }
-  uri += path;
-  return uri;
+    if (ui.loginAsUser->isChecked()) {
+        const QString user = ui.userName->text().trimmed();
+        if (!user.isEmpty()) {
+            uri += user;
+            uri += QLatin1Char('@');
+        }
+    }
+
+    uri += ui.host->text().trimmed();
+
+    const int port = ui.port->value();
+    if (port != serverType.defaultPort) {
+        uri += QLatin1Char(':');
+        uri += QString::number(port);
+    }
+
+    QString path = ui.path->text();
+    if (path.isEmpty() || path.at(0) != QLatin1Char('/')) {
+        uri += QLatin1Char('/');
+    }
+    uri += path;
+
+    return uri;
 }
 
-void ConnectServerDialog::onCurrentIndexChanged(int /*index*/) {
-  int serverTypeIdx = ui.serverType->currentIndex();
-  const auto& serverType = serverTypes[serverTypeIdx];
-  ui.port->setValue(serverType.defaultPort);
-  ui.anonymousLogin->setEnabled(serverType.canAnonymous);
-  if(serverType.canAnonymous)
-    ui.anonymousLogin->setChecked(true);
-  else
-    ui.loginAsUser->setChecked(true);
-  ui.host->setFocus();
-  checkInput();
+void ConnectServerDialog::onCurrentIndexChanged(int index) {
+    int serverTypeIdx = index;
+    if (serverTypeIdx < 0 || serverTypeIdx >= serverTypes.size()) {
+        serverTypeIdx = ui.serverType->currentIndex();
+    }
+    if (serverTypeIdx < 0 || serverTypeIdx >= serverTypes.size()) {
+        checkInput();
+        return;
+    }
+
+    const auto& serverType = serverTypes[serverTypeIdx];
+
+    ui.port->setValue(serverType.defaultPort);
+    ui.anonymousLogin->setEnabled(serverType.canAnonymous);
+
+    if (serverType.canAnonymous) {
+        ui.anonymousLogin->setChecked(true);
+    } else {
+        ui.loginAsUser->setChecked(true);
+    }
+
+    ui.host->setFocus();
+    checkInput();
 }
 
 void ConnectServerDialog::checkInput() {
-  bool valid = true;
-  if(ui.host->text().trimmed().isEmpty()) {
-    valid = false;
-  }
-  else if(ui.loginAsUser->isChecked() && ui.userName->text().trimmed().isEmpty()) {
-    valid = false;
-  }
-  ui.buttonBox->button(QDialogButtonBox::Ok)->setEnabled(valid);
+    bool valid = true;
+
+    const QString hostText = ui.host->text().trimmed();
+    const QString userText = ui.userName->text().trimmed();
+
+    if (hostText.isEmpty()) {
+        valid = false;
+    } else if (ui.loginAsUser->isChecked() && userText.isEmpty()) {
+        valid = false;
+    }
+
+    if (auto* okButton = ui.buttonBox->button(QDialogButtonBox::Ok)) {
+        okButton->setEnabled(valid);
+    }
 }
 
-} // namespace PCManFM
+}  // namespace PCManFM
