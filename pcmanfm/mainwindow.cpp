46a92e7a92983b9f08fa3ad873934861c858d61b
Paul Moses <p@1g4.org>
2026-01-28 23:12:44 -0600

    refactor(pcmanfm): disable statusbar size grip
    Disable the size grip on the status bar to prevent accidental resizing and present a cleaner UI for all users, including root.
    
    "Life is short but code can be long."

--- pcmanfm/mainwindow.cpp
+++ pcmanfm/mainwindow.cpp
@@ -75,7 +75,7 @@ MainWindow::MainWindow(Panel::FilePath path)
     fsInfoLabel_->setVisible(false);
 
     if (ui.statusbar) {
-        ui.statusbar->setSizeGripEnabled(true);
+        ui.statusbar->setSizeGripEnabled(false);
         ui.statusbar->addPermanentWidget(fsInfoLabel_);
 
         if (geteuid() == 0) {
